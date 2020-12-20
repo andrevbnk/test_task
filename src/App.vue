6104d994c4531ff201ae72755df300e200ab0f77
Andrey Morozov <andrevbnk72@gmail.com>
2020-12-21 00:24:03 +0200

    Добавление тестового задания

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/style/variables.scss';
+
+#app {
+  font-family: $font-family;
+  font-size: 14px;
+  a {
+    text-decoration: none;
+    &.router-link-exact-active {
+      color: $secondary-color;
+    }
+  }
+  a:hover{
+    color:$primary-color;
+  }
+  a:active {
+    color: $secondary-color;
+   } 
+
+  h1{
+    font-size: 50px;
+    line-height: 54px;
+    font-weight: 400;
+  }
+  h2{
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 400;
+  }
+  .paragraf{
+    display:block;
+    width: 100%;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+  }
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+<header>
+
+  <nav class="navbar padding fixed-top navbar-expand-lg navbar-light scrolling-navbar">
+    <img src='../assets/logo.svg' alt="">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">About me<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Relationships</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Requirements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Users</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Sign Up</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scopedSlots>
+@import '@/style/variables.scss';
+.navbar.padding{
+    padding: 10px;
+    padding-left: 30px;
+}
+.nav-item{
+    padding-top: 2px;
+    margin-right: 12.5px;
+    float: right;
+}
+.navbar{
+    background-color:$background-color;
+}
+</style>
